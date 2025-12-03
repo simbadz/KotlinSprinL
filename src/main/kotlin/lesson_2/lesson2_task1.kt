465fcb353a2c8aade21004f3fa5d5f1c34ab8fd7
simbadz <simbadez@yandex.ru>
2025-12-03 20:20:30 +0300

    KS-2-1 task1

---
+++ src/main/kotlin/lesson_2/lesson2_task1.kt
@@ -0,0 +1,12 @@
+package org.example.lesson_2
+
+fun main() {
+    val studentPoints1 = 3
+    val studentPoints2 = 4
+    val studentPoints3 = 3
+    val studentPoints4 = 5f
+
+    val averageScore = (studentPoints1 + studentPoints2 + studentPoints3 + studentPoints4) / 4
+
+    println(averageScore)
+}
