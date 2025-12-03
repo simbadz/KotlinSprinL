5dd4ba1513172e2d04a739a905414e891fb10dda
simbadz <simbadez@yandex.ru>
2025-12-03 18:51:12 +0300

    KS-1-4 refactor

--- src/main/kotlin/variables_lesson1/lesson1_task3.kt
+++ src/main/kotlin/variables_lesson1/lesson1_task3.kt
@@ -1,15 +1,15 @@
 package org.example.variables_lesson1
 
 fun main() {
-    val year: Int = 1961
-    var hour: Int = 9
-    var minute: Int = 7
+    val year: String = "1961"
+    var hour: String = "09"
+    var minute: String = "07"
 
     println(year)
     println(hour)
     println(minute)
 
-    hour = 10
-    minute = 55
+    hour = "10"
+    minute = "55"
     print("$hour:$minute")
 }
