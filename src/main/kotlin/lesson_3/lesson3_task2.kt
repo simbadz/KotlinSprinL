cf157d98785ebe2f2ab9968327184e54cfa10a2e
simbadz <simbadez@yandex.ru>
2025-12-04 12:39:12 +0300

    KS-3-2 task2

---
+++ src/main/kotlin/lesson_3/lesson3_task2.kt
@@ -0,0 +1,15 @@
+package org.example.lesson_3
+
+fun main() {
+    val lastName = "Андреева"
+    val lastNameWedding = "Сидорова"
+    val name = "Татьяна"
+    val middleName = "Сергеевна"
+    var age: String
+
+    age = "20"
+    println("$lastName $name $middleName, $age лет")
+
+    age = "22"
+    println("$lastNameWedding $name $middleName, $age года")
+}
