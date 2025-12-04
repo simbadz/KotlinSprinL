77ee50d3d86be3bc1575f58f8fc9e90bbca29c7d
simbadz <simbadez@yandex.ru>
2025-12-04 20:49:28 +0300

    KS-2-3 task3 refactor

--- src/main/kotlin/lesson_2/lesson2_task3.kt
+++ src/main/kotlin/lesson_2/lesson2_task3.kt
@@ -1,13 +1,17 @@
 package org.example.lesson_2
 
+const val minutesInHour = 60
+
 fun main() {
-    val timeOfTrainStart = 579
+    val timeOfTrainStartRaw = "9:39"
     val timeOfTrainTravel = 457
 
+    val (hours, minutes) = timeOfTrainStartRaw.split(":").map { it.toInt() }
+    val timeOfTrainStart = hours * minutesInHour + minutes
     val timeOfArrival = timeOfTrainStart + timeOfTrainTravel
 
-    val hoursOfArrivalFormat = timeOfArrival / 60
-    val minutesOfArrivalFormat = hoursOfArrivalFormat % 60
+    val hoursOfArrivalFormat = timeOfArrival / minutesInHour
+    val minutesOfArrivalFormat = hoursOfArrivalFormat % minutesInHour
     val formattedTime = String.format("%02d:%02d", hoursOfArrivalFormat, minutesOfArrivalFormat)
     println(formattedTime)
 
