2dad9a750c9c47867ed7a47e7aad23c5826c4c16
simbadz <simbadez@yandex.ru>
2025-12-04 20:30:14 +0300

    KS-3-2 task2 refactor

--- src/main/kotlin/lesson_2/lesson2_task1.kt
+++ src/main/kotlin/lesson_2/lesson2_task1.kt
@@ -5,8 +5,9 @@ fun main() {
     val studentPoints2 = 4
     val studentPoints3 = 3
     val studentPoints4 = 5f
+    val studentsAmount = 4
 
-    val averageScore = (studentPoints1 + studentPoints2 + studentPoints3 + studentPoints4) / 4
+    val averageScore = (studentPoints1 + studentPoints2 + studentPoints3 + studentPoints4) / studentsAmount
 
     println(averageScore)
 }
