86d981d5b8004d390711030093bbda5bd70a5779
simbadz <simbadez@yandex.ru>
2025-12-04 21:00:21 +0300

    KS-2-4 task5 refactor

--- src/main/kotlin/lesson_2/lesson2_task4.kt
+++ src/main/kotlin/lesson_2/lesson2_task4.kt
@@ -3,8 +3,9 @@ package org.example.lesson_2
 fun main() {
     val crystallineOre = 7
     val ironOre = 11
+    val buffPercent = 20
 
-    val buff = 1.2
+    val buff = 1 + buffPercent / 100.0
     val crystallineOreBuff = (crystallineOre * buff) - crystallineOre
     val ironOreBuff = (ironOre * buff) - ironOre
 
