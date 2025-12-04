3c6edc5f6dfd7078471aafda984c0788bcc9be38
simbadz <simbadez@yandex.ru>
2025-12-04 20:52:39 +0300

    KS-1-5 task5 refactor2

--- src/main/kotlin/variables_lesson1/lesson1_task5.kt
+++ src/main/kotlin/variables_lesson1/lesson1_task5.kt
@@ -1,14 +1,14 @@
 package org.example.variables_lesson1
 
-const val secondsInHour = 3600
-const val minutesInHour = 60
+const val SECONDS_IN_HOUR = 3600
+const val MINUTES_IN_HOUR = 60
 
 fun main() {
     val totalSecondsInSpaceUraGagarin: Int = 6480
 
-    val hours: Int = totalSecondsInSpaceUraGagarin / secondsInHour
-    val minutes: Int = (totalSecondsInSpaceUraGagarin % secondsInHour) / minutesInHour
-    val seconds: Int = totalSecondsInSpaceUraGagarin % minutesInHour
+    val hours: Int = totalSecondsInSpaceUraGagarin / SECONDS_IN_HOUR
+    val minutes: Int = (totalSecondsInSpaceUraGagarin % SECONDS_IN_HOUR) / MINUTES_IN_HOUR
+    val seconds: Int = totalSecondsInSpaceUraGagarin % MINUTES_IN_HOUR
 
     val timeFormat = "%02d:%02d:%02d".format(hours, minutes, seconds)
     println(timeFormat)
